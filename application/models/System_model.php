d93e8e390acef4e7ec68809e282731d905f3c26a
zorexso <madza204@gmail.com>
2020-02-25 15:19:57 +0700

    gimana coba

---
+++ application/models/System_model.php
@@ -0,0 +1,58 @@
+<?php
+
+class System_model extends CI_Model {
+
+	function select_all($table)
+	{	
+		return $this->db->get($table)->result_array();
+	}
+
+	function get_by_atr($table, $where)
+	{
+		return $this->db->get_where($table,$where);
+	}
+
+	function get_sum($table, $atr)
+	{
+		$temp = $this->db->query("select sum($atr) as sum from $table");
+		return $temp->result();
+	}
+
+	function get_sum_where($table, $atr, $where)
+	{
+		$temp = $this->db->query("select sum($atr) as sum where $where from $table");
+		return $temp->result();
+	}
+
+	function get_count($table, $atr)
+	{
+		$temp = $this->db->query("SELECT COUNT($atr) AS $atr FROM $table");
+		return $temp->result();
+	}
+
+	function get_count_where($table, $atr, $where)
+	{
+		$temp = $this->db->query("select count($atr) as $atr from $table where $where");
+		return $temp->result();
+	}
+
+	function add($data, $table)
+	{
+		$this->db->insert($table, $data);	
+	}
+
+	function update($where, $data, $table)
+	{
+		$this->db->where($where);
+		$this->db->update($table, $data);
+	}
+
+	function delete($table, $where)
+	{
+		//menghapus suatu baris dari suatu tabel
+		$this->db->delete($table, $where);
+	}
+}
+
+//fk
+//end of file
