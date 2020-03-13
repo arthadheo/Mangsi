e2026260634553fa1a52b1fc4946b3bf5e14e854
zorexso <madza204@gmail.com>
2020-03-13 09:56:05 +0700

    nambahin logo mangsi

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['404_override'] = 'error/cli/error_404';
+$route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
 
 $route['login'] = 'welcome/login';
