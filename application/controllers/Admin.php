a6f6cb0f92f40acf22599dd36b7def17ba4b4319
ozann <ahmadfauzan1198@gmail.com>
2020-10-08 11:12:53 +0700

    add page coupon dan user admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,28 +6,28 @@ class Admin extends CI_Controller
 	public function index()
 	{
 		$this->load->view('layout/adm_header');
-		$this->load->view('admin/dashboard');
+		$this->load->view('admin/adm_dashboard');
 		$this->load->view('layout/adm_footer');
 	}
 
 	public function user()
 	{
 		$this->load->view('layout/adm_header');
-		$this->load->view('admin/user');
+		$this->load->view('admin/adm_user');
 		$this->load->view('layout/adm_footer');
 	}
 
 	public function user_detail()
 	{
 		$this->load->view('layout/adm_header');
-		$this->load->view('admin/user_detail');
+		$this->load->view('admin/adm_user_detail');
 		$this->load->view('layout/adm_footer');
 	}
 
 	public function coupon()
 	{
 		$this->load->view('layout/adm_header');
-		$this->load->view('admin/coupon');
+		$this->load->view('admin/adm_coupon');
 		$this->load->view('layout/adm_footer');
 	}
 }
--- application/views/layout/adm_header.php
+++ application/views/layout/adm_header.php
@@ -13,7 +13,7 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('dist/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="<?= base_url('dist/'); ?>https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('dist/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
@@ -41,7 +41,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="<?= base_url(); ?>admin">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
@@ -51,7 +51,7 @@
 
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
-                <a class="nav-link" href="index.html">
+                <a class="nav-link" href="<?= base_url(); ?>adm_user">
                     <i class="fas fa-fw fa-user"></i>
                     <span>User</span></a>
                 </a>
